353e43f570148d2571333d1c9a4e84dd9402885a
Kenji Greene <kenjigrr@gmail.com>
2021-12-03 15:37:06 -0600

    created project middleware

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -1,1 +1,26 @@
 // add middlewares here related to projects
+const Project = require('./projects-model');
+
+async function validateProjectId (req,res,next){
+    try{
+        const user = await Project.get(req.params.id);
+        if(user){
+          req.user = user
+        }else{
+          next({status: 404, message: 'user not found'});
+        }
+      }catch (err){
+        next(err)
+      }
+}
+
+function validateProjectPayload (req,res,next){
+    const { name, description, completed } = req.body;
+    if (!name || !description || !completed) {
+        res.status(400).json({ message: 'missing required name field' });
+    } else {
+    next();
+  }
+}
+
+module.exports = {validateProjectId, validateProjectPayload}
