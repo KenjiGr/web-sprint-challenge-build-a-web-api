7c419b69d75cd3ca20748a98ef6ad62ef6a0c1fc
Kenji Greene <kenjigrr@gmail.com>
2021-12-03 15:22:54 -0600

    npm i & set up code for api

--- api/server.js
+++ api/server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const server = express();
 
+const projectRouter = require('./projects/projects-router');
+server.use('/api/projects', projectRouter);
+const actionRouter = require('./actions/actions-router');
+server.use('/api/actions', actionRouter);
 // Configure your server here
 // Build your actions router in /api/actions/actions-router.js
 // Build your projects router in /api/projects/projects-router.js
